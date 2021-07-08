40eae0f614eda38059c76d3643af9a76bc94273b
ichigo-dev <ichigo@web-freework.com>
2021-07-09 00:57:02 +0900

    add: src/data

---
+++ src/data/index.ts
@@ -0,0 +1,23 @@
+type Member = {
+   id: number,
+   name: string,
+   age: number
+};
+
+const MemberList: Member[] = [
+  {
+    id: 1,
+    name: 'hoge',
+    age: 20,
+  },
+  {
+    id: 2,
+    name: 'fuga',
+    age: 30,
+  },
+  {
+    id: 3,
+    name: 'hogefuga',
+    age: 40,
+  },
+];
